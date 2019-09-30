6376bc2f7cb069ae406c0564d7feff29bb0293ee
arthur <sabatinoarthur@gmail.com>
2019-09-30 12:05:20 -0300

    affs

--- src/app/diario/diario-salvar/diario-salvar.component.ts
+++ src/app/diario/diario-salvar/diario-salvar.component.ts
@@ -3,14 +3,15 @@ import { Diario } from '../entidade/diario';
 import { AngularFireDatabase } from '@angular/fire/database';
 
 
+
 @Component({
   selector: 'app-diario-salvar',
   templateUrl: './diario-salvar.component.html',
   styleUrls: ['./diario-salvar.component.scss'],
 })
 export class DiarioSalvarComponent implements OnInit {
-	
-  	
+
+
   diario: Diario = new Diario();
 
   constructor(private banco: AngularFireDatabase,) { }
@@ -18,7 +19,7 @@ export class DiarioSalvarComponent implements OnInit {
   ngOnInit() { }
 
   diarioSalvar() {
-	dateFormat(this.diario.hentrada, "h:MM:ss TT"); 	
+
     this.banco.list('diario').push(this.diario);
     this.diario = new Diario();
     alert("Diario Salvo");
