96514aa9bf82498346cdbc74776b216e7e4dcc0b
arthur <sabatinoarthur@gmail.com>
2019-10-01 15:28:58 -0300

    blah

--- src/app/diario/relatorio/relatorio.page.ts
+++ src/app/diario/relatorio/relatorio.page.ts
@@ -5,6 +5,10 @@ import { map } from 'rxjs/operators'
 import { Projeto } from 'src/app/projeto/entidade/projeto';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { Router } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-relatorio',
@@ -12,22 +16,23 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
   styleUrls: ['./relatorio.page.scss'],
 })
 export class RelatorioPage implements OnInit {
-
+  email: string = "";
+  mensagem: string = "";
   listaDiario: Observable<Diario[]>;
   listaProjetos: Observable<Projeto[]>;
-//private fire: AngularFireDatabase, private banco: AngularFireDatabase
-  constructor(public emailComposer: EmailComposer) {
-  /*  this.listaDiario = this.fire.list<Diario>('diario').snapshotChanges().pipe(
+  //private fire: AngularFireDatabase, private banco: AngularFireDatabase
+  constructor(public emailComposer: EmailComposer, private fire: AngularFireDatabase, private rota: Router) {
+    this.listaDiario = this.fire.list<Diario>('diario').snapshotChanges().pipe(
       map(lista => lista.map(linha => ({
         key: linha.payload.key, ...linha.payload.val()
       })))
     );
 
-    this.listaProjetos = this.banco.list<Projeto>('projeto').snapshotChanges().pipe(
+    this.listaProjetos = this.fire.list<Projeto>('projeto').snapshotChanges().pipe(
       map(lista => lista.map(linha => ({
         key: linha.payload.key, ...linha.payload.val()
       })))
-    );*/
+    );
 
 
 
@@ -40,28 +45,29 @@ export class RelatorioPage implements OnInit {
 
 
 
-
-
-enviar(){
-
-  let email = {
-    to: 'sabatinoarthur@gmail.com',
-    cc: '',
-    bcc: [''],
-    attachments: [
-      ''
-    ],
-    subject: 'Cordova Icons',
-    body: 'How are you? Nice greetings from Leipzig',
-    isHtml: true,
-    app: 'gmail',
+  voltar(){
+    this.rota.navigate(['diariol']) ;
   }
 
-  // Send a text message using default options
-  this.emailComposer.open(email);
+  enviar() {
+
+    let email = {
+      to: this.email,
+      cc: '',
+      bcc: [''],
+      attachments: [
+        ''
+      ],
+      subject: 'Cordova Icons',
+      body: "Relatório de Projeto " + this.listaDiario + " .",
+      isHtml: true,
+    }
+
+    // Send a text message using default options
+    this.emailComposer.open(email);
 
 
-}
+  }
 
 
 
